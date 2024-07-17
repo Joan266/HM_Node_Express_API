6cbdbe53a272342988c29ac68719a97c7b0b6932
Joan <joanalemany26@gmail.com>
2024-07-17 12:37:45 +0200

    app.ts refactoring

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,27 @@
-import express, { Express } from 'express';
-// import { authenticateToken } from './middleware/auth';
+import express, { Express, Request, Response } from 'express';
+import { authenticateToken } from './middleware/auth';
 
-// import roomController from './controllers/roomController';
-// import userController from './controllers/userController';
-// import bookingController from './controllers/bookingController';
-// import contactController from './controllers/contactController';
-import loginController from './controllers/login';
+import employeeController from './controllers/employee'; 
 
 export const app: Express = express();
 
-// Settings
-app.set('port', 3000);
+app.set('port', process.env.PORT || 3000); 
 
-// Middlewares
 app.use(express.json());
 
-app.use('/login', loginController);
-// app.use('/room', authenticateToken, roomController);
-// app.use('/user', authenticateToken, userController);
-// app.use('/booking', authenticateToken, bookingController);
-// app.use('/contact', authenticateToken, contactController);
+
+app.use('/employees/login', employeeController); 
+
+app.use(authenticateToken);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('API Miranda\nRoutes: /rooms, /bookings, /employees, /reviews');
+});
+
+app.use('/employee', employeeController); 
+
+app.use('*', (req: Request, res: Response) => {
+    res.status(404).send('Route not found');
+});
+
+export default app;
